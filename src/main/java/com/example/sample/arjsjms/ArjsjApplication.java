bbbac7ffcac8fbaa502317754daec3e4fcaab2af
gary <garylaw022722@gmail.com>
2024-07-03 20:28:49 +0800

    save All

---
+++ src/main/java/com/example/sample/arjsjms/ArjsjApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample.arjsjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArjsjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArjsjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sample/arjsjms/ArjsjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample.arjsjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArjsjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
